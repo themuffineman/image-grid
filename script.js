cc284a26ee1bbcc981423dbb947c46be0c9a4faf
Petrus <petrusheya@gmail.com>
2023-12-29 23:24:05 +0200

    Async code problem fixed

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const canvas = document.querySelector("canvas");
-console.log(canvas)
 const ctx = canvas.getContext("2d");
+let imageData = 0;
 
-ctx.fillStyle = "yellow";
-ctx.fillRect(50, 50, 1, 2);
+const img = new Image();
 
-const imageData = ctx.getImageData(50, 50, 3, 2);
-console.log(imageData)
-const myImageData = ctx.createImageData(100, 50);
-console.log(myImageData)
+img.src = 'horses-desktop.jpg'
+
+img.onload = function() {
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    console.log(imageData)
+};
+console.log(img)
+
+
+
+
+
+
+
+
